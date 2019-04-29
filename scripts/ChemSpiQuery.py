a7d831d46373b0eda5930808d25754f03a04acba
jtsorren <jtsorren@eng.ucsd.edu>
2019-04-28 21:01:16 -0700

    Chemical Spider Query for Compound
    python3
    API only allows 1000 queries per key :(

---
+++ scripts/ChemSpiQuery.py
@@ -0,0 +1,26 @@
+from chemspipy import ChemSpider
+cs = ChemSpider('<YOUR-API-KEY>')
+import pandas as pd
+
+def get_SMILES(compounds):
+    smiles = []
+    for compound in compounds:
+        try:
+            float(compound)
+            smiles.append(compound)
+        except:
+            result = cs.search(compound)
+            smiles.append([i.smiles for i in result])
+    return smiles
+
+if __name__ == '__main__':
+    inhib_df = pd.read_csv('../data/inhibitors.csv')
+    activ_df = pd.read_csv('../data/activators.csv')
+    inhib_smiles_df = pd.DataFrame(columns=inhib_df.columns)
+    for column in inhib_df.columns:
+        inhib_smiles_df[column] = get_SMILES(inhib_df[column])
+    activ_smiles_df = pd.DataFrame(columns=activ_df.columns)
+    for column in activ_df.columns:
+        activ_smiles_df[column] = get_SMILES(activ_df[column])
+    inhib_smiles_df.to_csv('../data/inhibitors_smiles.csv', index=False)
+    activ_smiles_df.to_csv('../data/activators_smiles.csv', index=False)
