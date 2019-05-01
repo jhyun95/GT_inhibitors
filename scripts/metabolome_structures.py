4968d9cd654c16d87cf5b55a28801a6a4b1783e9
Jason Hyun <jhyun95@gmail.com>
2019-05-01 04:32:38 -0700

    Added structures for 1143 CHO metabolites from ChEBI

--- scripts/metabolome_structures.py
+++ scripts/metabolome_structures.py
@@ -6,6 +6,7 @@ Created on Tue Apr 30 14:47:27 2019
 @author: jhyun95
 """
 
+import os, urllib
 import cobra
 import pandas as pd
 
@@ -24,6 +25,9 @@ NAME_FIXES = { # for BiGG IDs that are on MetaNetX but not iCHOv1?
     'm3macchitppdol': '(alpha-D-mannosyl)3-beta-D-mannosyl-diacetylchitodiphosphodolichol',
     'ptd2meeta_SC': 'phosphatidyldi-N-methylethanolamine_tomerge'}
 
+db_priorities = ['chebi', 'hmdb', 'kegg', 'lipidmaps'] # structures readily available
+db_priorities += ['seed', 'metacyc', 'sabiork', 'reactome', 'mnxm', 'bigg'] # structures less available
+
 def process_metanetx(mnxm_path=METABOLITES_FILE, 
                      model_path=MODEL_FILE,
                      out_path=METABOLITE_TAG_FILE):
@@ -31,8 +35,6 @@ def process_metanetx(mnxm_path=METABOLITES_FILE,
     df = pd.read_csv(mnxm_path, header=None, delimiter='\t')
     print('Loaded MetaNetX output:', df.shape)
     model = cobra.io.load_json_model(model_path)
-    db_priorities = ['chebi', 'hmdb', 'kegg', 'lipidmaps'] # structures readily available
-    db_priorities += ['seed', 'metacyc', 'sabiork', 'reactome', 'mnxm', 'bigg'] # structures less available
     compartments = model.compartments.keys()
     db_links = {}
     
@@ -83,4 +85,65 @@ def process_metanetx(mnxm_path=METABOLITES_FILE,
     df_db_links.to_csv(out_path)
     return df_db_links
 
-df = process_metanetx()
+def download_mol_files(output_dir=OUTPUT_DIR, tag_file=METABOLITE_TAG_FILE, 
+                      limit=-1, db_order=['chebi'], show_all=False):
+    ''' Download structures based on the tag_file. Capable of querying ChEBI
+        (TODO: HMDB, KEGG, and LipidMaps querying) '''
+    df = pd.read_csv(tag_file, index_col=0) 
+    existing = list(map(lambda x: x[:-4], os.listdir(output_dir)))
+    print('Total Metabolites:', df.shape[0])
+    print('    Downloaded:', len(existing))
+    print('    Missing:', df.shape[0] - len(existing), '\n')
+    counter = 0
+    for met in df.index:
+        mol_file = output_dir + '/' + met + '.mol'
+        mol_file = mol_file.replace('//', '/')
+        counter += 1; found = False
+        if met in existing:
+            if show_all:
+                print(met, 'already downloaded')
+        else: # otherwise, try to get structure from any of the allowable DBs
+            for db in db_order: # test allowable DBs
+                if not pd.isnull(df.loc[met,db]): # if a record exists for this DB
+                    for tag in df.loc[met,db].split(';'): # test all possible tags
+                        ''' Convert the tag into a url'''
+                        url = None
+                        if db == 'chebi':
+                            url = 'https://www.ebi.ac.uk/chebi/saveStructure.do?'
+                            url +='defaultImage=true&chebiId=' + str(tag) + '&imageId=0'
+                        elif db == 'hmdb':
+                            pass # TODO
+                        elif db == 'kegg':
+                            pass # TODO
+                        elif db == 'lipidmaps':
+                            pass # TODO
+                        
+                        ''' Query the url to download the structure '''
+                        if not url is None:
+                            print(met, '\t', url)
+                            
+                            data = urllib.request.urlopen(url, timeout=10)
+                            output = ''
+                            for bytestring in data:
+                                line = bytestring.decode("utf-8") 
+                                output += line
+                            if len(output.strip()) > 0: # got something
+                                f = open(mol_file, 'w+')
+                                f.write(output)
+                                f.close()
+                                found = True
+    
+                        ''' Stop testing tags if structure was found '''
+                        if found: 
+                            break
+                        
+                ''' Stop testing databases if structure was found '''
+                if found: 
+                    break
+                    
+        if counter >= limit and limit != -1:
+            break;
+            
+
+#df = process_metanetx()
+download_mol_files()
