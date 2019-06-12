9115f9ed2c20621c43b35c4e71b36ff31dee6fc9
jtsorren <jtsorren@ucsc.edu>
2019-06-11 18:13:57 -0700

    scaffold fingerprinting using sng

---
+++ scripts/scaffold_fingerprinting_inhibitors.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import subprocess
+import networkx as nx
+
+if __name__ == '__main__':
+    inhibitor_smiles = pd.read_csv('../data/inhibitor_smiles.tsv', sep='\t')
+    inhibitor_smiles_processed = inhibitor_smiles[['SMILES', 'group_id']]
+    inhibitor_smiles_processed.to_csv('../data/temp.smi', sep='\t', header=False, index=False)
+    inhibitor_generate = subprocess.run(['sng', 'generate', '-p', '100', '-o', '../data/temp.tmp', '../data/temp.smi'], check=True, text=True)
+    inhibitor_aggregate = subprocess.run(['sng', 'aggregate', '-m', '../data/temp_m.tmp', '-o', '../data/temp.network', '../data/temp.tmp'], check=True, text=True)
+    network_df = pd.read_csv('../data/temp.network', sep='\t')
+    molecule_map = pd.read_csv('../data/temp_m.tmp', sep='\t')
+    G = nx.DiGraph()
+    G.add_nodes_from([str(i) for i in list(network_df['ID'])])
+    for index in network_df.index:
+        source = str(network_df['ID'][index])
+        try:
+            targets = [i for i in network_df['SUBSCAFFOLDS'][index].split(',') if len(i)]
+            for target in targets:
+                G.add_edges_from([(source, str(target))])
+        except: 
+            pass
+    molecules = set(molecule_map['MOLECULE_ID'])
+    scaffold_d = dict(zip(set(network_df['SMILES']), [0]*len(set(network_df['SMILES']))))
+    molecule_scaffold_d = dict(zip(molecules,[scaffold_d.copy() for i in range(len(molecules))]))
+    for index in molecule_map.index:
+        molecule = molecule_map['MOLECULE_ID'][index]
+        source = str(molecule_map['SCAFFOLD_ID'][index])
+        descendants =  nx.descendants(G, source)
+        molecule_scaffold_d[molecule][list(network_df.loc[network_df['ID'] == int(source)]['SMILES'])[0]] = 1
+        for descendant in descendants:
+            molecule_scaffold_d[molecule][list(network_df.loc[network_df['ID'] == int(descendant)]['SMILES'])[0]] = 1
+    removal = subprocess.run(['rm', '-r', '../data/temp.smi', '../data/temp.tmp', '../data/temp.network', '../data/temp_m.tmp'], check=True, text=True)
+    pd.DataFrame(data=molecule_scaffold_d).to_csv('../data/scaffold_fingerprints_inhibitors.csv')
